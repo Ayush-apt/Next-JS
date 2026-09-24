a3a9ad58b240b252df6cd15cc4f9cc5589e320b9
Ashu <ayushprajapati272007@gmail.com>
2026-09-24 23:30:35 +0530

    TypeScript Basics

---
+++ level_04/app/tutorial.ts
@@ -0,0 +1,28 @@
+// let a:number;
+// a = 99
+
+// let b :string;
+// b = "heyyo"
+
+// let online:boolean;
+// online = false
+
+// let arr:number[];
+// arr = [23,542,11,68]
+
+// let arr1:[number, string];
+// arr1 = [77, "Meoww"]
+
+// enum role {
+//     "user", "admin"
+// }
+
+
+let a:any;
+a = 90;
+a = "Ashu";
+a = true;
+
+function Ashu(a:number, b:string = "Ashu"){
+    return false;
+}
